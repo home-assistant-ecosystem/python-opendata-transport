fbe4133184a9b6c096e5be3bca49c57221dc8965
Fabian Affolter <mail@fabian-affolter.ch>
2017-08-23 16:28:22 +0200

    Add classifiers and update for 0.0.2

--- setup.py
+++ setup.py
@@ -14,14 +14,25 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='python_opendata_transport',
-    version='0.0.1',
+    version='0.0.2',
     description='Python API for interacting with transport.opendata.ch.',
     url='https://github.com/fabaff/python-opendata-transport',
     download_url='https://github.com/fabaff/python-opendata-transport/releases',
     author='Fabian Affolter',
     author_email='fabian@affolter-engineering.ch',
     license='MIT',
-    install_requires=['requests>=2.0', 'pytz'],
+    install_requires=['aiohttp'],
     packages=['opendata_transport'],
     zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+    ],
 )
