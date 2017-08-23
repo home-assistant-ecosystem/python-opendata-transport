06abf13073e68a1e40041979914d344f75a4eee3
Fabian Affolter <mail@fabian-affolter.ch>
2017-08-23 16:19:40 +0200

    Update for async

--- example.py
+++ example.py
@@ -3,18 +3,26 @@ Copyright (c) 2015-2017 Fabian Affolter <fabian@affolter-engineering.ch>
 
 Licensed under MIT. All rights reserved.
 """
+import asyncio
+import aiohttp
+
 from opendata_transport import OpendataTransport
 
-data = OpendataTransport('Bern', 'Biel')
 
-# Get the data
-data.get_data()
+@asyncio.coroutine
+def main():
+    with aiohttp.ClientSession() as session:
+        data = OpendataTransport('Bern', 'Biel', loop, session)
+        yield from data.async_get_data()
 
-# Print the start and the destination name
-print("Train connections:", data.from_name, "->", data.to_name)
+        # Print the start and the destination name
+        print("Train connections:", data.from_name, "->", data.to_name)
 
-# Print the next three connections
-print(data.connections)
+        # Print the next three connections
+        print(data.connections)
 
-# Print the details of the next connection
-print(data.connections[0])
+        # Print the details of the next connection
+        print(data.connections[0])
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
