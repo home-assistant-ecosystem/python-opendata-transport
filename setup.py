a1d28b692b98448ff31ae42b43e479a40d17be03
Fabian Affolter <fabian@affolter-engineering.ch>
2018-06-09 11:05:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ Licensed under MIT. All rights reserved.
 import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 if sys.argv[-1] == 'publish':
     os.system('python3 setup.py sdist upload')
@@ -18,7 +21,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='python_opendata_transport',
-    version='0.1.1',
+    version='0.1.2',
     description='Python API for interacting with transport.opendata.ch.',
     url='https://github.com/fabaff/python-opendata-transport',
     download_url='https://github.com/fabaff/python-opendata-transport/releases',
@@ -26,7 +29,7 @@ setup(
     author_email='fabian@affolter-engineering.ch',
     license='MIT',
     install_requires=['aiohttp', 'async_timeout'],
-    packages=['opendata_transport'],
+    packages=find_packages(),
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
