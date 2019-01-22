f8f5e9bb5774a7668a65756d8d0b057f891c4003
Fabian Affolter <fabian@affolter-engineering.ch>
2019-01-22 23:41:52 +0100

    Update docstring

--- example.py
+++ example.py
@@ -1,8 +1,4 @@
-"""
-Copyright (c) 2015-2018 Fabian Affolter <fabian@affolter-engineering.ch>
-
-Licensed under MIT. All rights reserved.
-"""
+"""Example to get the details for a connection."""
 import asyncio
 import aiohttp
 
