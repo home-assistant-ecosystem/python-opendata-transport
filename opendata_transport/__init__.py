a4fd539299e2632341e13b89eda469969f83acd4
Stefan Agner <stefan@agner.ch>
2019-12-31 00:23:44 +0100

    Add stationboard support (#9)
    * introduce new base class OpendataTransportBase
    
    This prepares for stationboard support.
    
    * add stationboard support
    
    * add station id support
    
    * support multiple stations
    
    Often there are multiple stations with a very similar name (e.g. with
    and without a comma) closeby which usually are considered a single
    station. Allow to combine the result of those stations in a single
    result.

--- opendata_transport/__init__.py
+++ opendata_transport/__init__.py
@@ -9,16 +9,88 @@ import urllib.parse
 from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
-_RESOURCE = "http://transport.opendata.ch/v1/"
+_RESOURCE_URL = "http://transport.opendata.ch/v1/"
 
+class OpendataTransportBase(object):
+    """Opendata Transport base class"""
 
-class OpendataTransport(object):
+    def __init__(self, loop, session):
+        self._loop = loop
+        self._session = session
+
+    def get_url(self, resource, params):
+        param = urllib.parse.urlencode(params)
+        url = "{resource_url}{resource}?{param}".format(
+                resource_url=_RESOURCE_URL, resource=resource, param=param)
+        return url
+
+class OpendataTransportStationboard(OpendataTransportBase):
+    """A class for handling stationsboards from Opendata Transport."""
+
+    def __init__(self, station, loop, session, limit=5):
+        """Initialize the journey."""
+        super().__init__(loop, session)
+        self.station = station
+        self.limit = limit
+        self.from_name = self.from_id = self.to_name = self.to_id = None
+        self.journeys = []
+
+    def __get_journey_dict(self, journey):
+        journeyinfo = dict()
+        journeyinfo["departure"] = journey["stop"]["departure"]
+        journeyinfo["delay"] = journey["stop"]["delay"]
+        journeyinfo["platform"] = journey["stop"]["platform"]
+        journeyinfo["name"] = journey["name"]
+        journeyinfo["category"] = journey["category"]
+        journeyinfo["number"] = journey["number"]
+        journeyinfo["to"] = journey["to"]
+
+        return journeyinfo
+
+    async def __async_get_data(self, station):
+        """Retrieve the data for the connection."""
+        params = { 'limit': self.limit }
+        if str.isdigit(station):
+            params['id'] = station
+        else:
+            params['station'] = station
+
+        url = self.get_url("stationboard", params)
+
+        try:
+            with async_timeout.timeout(5, loop=self._loop):
+                response = await self._session.get(url, raise_for_status=True)
+
+            _LOGGER.debug("Response from transport.opendata.ch: %s", response.status)
+            data = await response.json()
+            _LOGGER.debug(data)
+        except asyncio.TimeoutError:
+            _LOGGER.error("Can not load data from transport.opendata.ch")
+            raise exceptions.OpendataTransportConnectionError()
+        except aiohttp.ClientError as aiohttpClientError:
+            _LOGGER.error("Response from transport.opendata.ch: %s", aiohttpClientError)
+            raise exceptions.OpendataTransportConnectionError()
+
+        try:
+            for journey in data["stationboard"]:
+                self.journeys.append(self.__get_journey_dict(journey))
+        except (TypeError, IndexError):
+            raise exceptions.OpendataTransportError()
+
+    async def async_get_data(self):
+        if isinstance(self.station, list):
+            for station in self.station:
+                await self.__async_get_data(station)
+            list.sort(self.journeys, key=lambda journey: journey["departure"])
+        else:
+            await self.__async_get_data(self.station)
+
+class OpendataTransport(OpendataTransportBase):
     """A class for handling connections from Opendata Transport."""
 
     def __init__(self, start, destination, loop, session, limit=3):
         """Initialize the connection."""
-        self._loop = loop
-        self._session = session
+        super().__init__(loop, session)
         self.limit = limit
         self.start = start
         self.destination = destination
@@ -46,10 +118,8 @@ class OpendataTransport(object):
 
     async def async_get_data(self):
         """Retrieve the data for the connection."""
-        param = urllib.parse.urlencode(
-                { 'from': self.start, 'to': self.destination,
-                  'limit': self.limit })
-        url = "{resource}connections?{param}".format(resource=_RESOURCE, param=param)
+        url = self.get_url("connections",
+            { 'from': self.start, 'to': self.destination, 'limit': self.limit })
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
