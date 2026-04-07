4922f19c18dde60ad3f017208554a72c240b8b2d
Fabian Affolter <fabian@affolter-engineering.ch>
2026-04-07 10:19:16 +0200

    Update Python version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "urllib3",
     ],
     packages=find_packages(),
-    python_requires=">=3.11",
+    python_requires=">=3.13",
     zip_safe=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -35,8 +35,8 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Utilities",
     ],
 )
